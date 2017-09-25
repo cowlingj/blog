a4ecf16405fd241670d176ee7c3fa55cc68d6127
Jonathan Cowling <09cowlingj@gmail.com>
2017-09-25 11:55:44 +0100

    FIX: cleaning routes, rou reduce the ammount they each contain

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,23 +6,19 @@ routes.get("/", function(req, res){
 })
 
 // auth.js
-routes.get("/register", require("./auth.js"))
-routes.post("/register", require("./auth.js"))
-routes.get("/login", require("./auth.js"))
-routes.post("/login", require("./auth.js"))
-routes.get("/logout", require("./auth.js"))
+routes.all("/register", require("./auth.js"))
+routes.all("/login", require("./auth.js"))
+routes.all("/logout", require("./auth.js"))
 
 // write.js
-routes.get("/write", require("./write.js"))
-routes.get("/edit/:id", require("./write.js"))
+routes.all("/write", require("./write.js"))
+routes.all("/edit*", require("./write.js"))
 
 // read.js
-routes.get("/read", require("./read.js"))
-routes.get("/read/:post", require("./read.js"))
+routes.all("/read*", require("./read.js"))
 
 // cookie.js
-routes.get("/cookie", require("./cookie"))
-routes.get("/cookie-parse", require("./cookie"))
+routes.all("/cookie*", require("./cookie"))
 
 // views.js
 routes.get("/react", require("./views.js"))
@@ -30,10 +26,7 @@ routes.get("/pug", require("./views.js"))
 routes.get("/handlebars", require("./views.js"))
 
 // user.js
-routes.post("/user/new", require("./user.js"))
-routes.get("/user/me", require("./user.js"))
-routes.get("/user", require("./user.js"))
-routes.get("/user/:id", require("./user.js"))
+routes.all("/user*", require("./user.js"))
 
 // 404
 routes.get("*", function(req, res, next){
